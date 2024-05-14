2df27ac4ff54914fe2efcc417c56a9bf102b8bb5
Jbasilio55 <j.l.basilio55@gmail.com>
2024-05-14 16:33:27 -0400

    Day 1 - Practice/ changes to languages files description

--- src/Week1/Practice/Intro/Languages.java
+++ src/Week1/Practice/Intro/Languages.java
@@ -6,17 +6,31 @@ public class Languages {
     public static void main(String[] args) {
         // Types of Languages [ Procedural | Functional | Object Oriented]
 
-        // Procedural
+        // ------ Procedural ------
         // - specifies a series of well-structured steps and procedures to compose a program.
         // - contains a systematic order of statements, functions and commands to complete a task.
 
-        // Functional
+        // ----- Functional ------
         // - Writing a program only pure functions i.e. never modify variables but only create new ones as an output.
         // - used in situations where we have to perform lots of different operations on the same set of data, like ML.
         // - First Class functions?
 
-        // Object Oriented
-        // Code + Data = Object
-        // Develop to make it easier to develop, debug, reuse, and maintain software
+        // ---- Object Oriented -----
+        // - Code + Data = Object
+        // - Develop to make it easier to develop, debug, reuse, and maintain software
+
+        // Static vs Dynamic Languages
+
+        // ----- Static ------
+        // - static perform type check at compile time
+        // - Errors will show at compile time
+        // - Declare datatype before you use it
+        // - More control
+
+        // ----- Dynamic ------
+        // - Perform type checking at runtime
+        // - Error might not show up until the program is run
+        // - No need to declare datatype or variables
+        // - Saves time in writing code but might throw an error at runtime
     }
 }
