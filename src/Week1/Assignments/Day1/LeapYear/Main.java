bddb2f7638e57cc32bbe7291becd776d31f9ec97
Jbasilio55 <j.l.basilio55@gmail.com>
2024-05-14 16:06:06 -0400

    Day 1 - Practice

---
+++ src/Week1/Assignments/Day1/LeapYear/Main.java
@@ -0,0 +1,13 @@
+package Week1.Assignments.Day1.LeapYear;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Input a year and find whether it is a leap year or not.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("----Leap Year checker----");
+        System.out.print("What Year would you like to check: ");
+    }
+}
---
+++ src/Week1/Practice/Intro/Languages.java
@@ -0,0 +1,22 @@
+package Week1.Practice.Intro;
+
+import java.util.Scanner;
+
+public class Languages {
+    public static void main(String[] args) {
+        // Types of Languages [ Procedural | Functional | Object Oriented]
+
+        // Procedural
+        // - specifies a series of well-structured steps and procedures to compose a program.
+        // - contains a systematic order of statements, functions and commands to complete a task.
+
+        // Functional
+        // - Writing a program only pure functions i.e. never modify variables but only create new ones as an output.
+        // - used in situations where we have to perform lots of different operations on the same set of data, like ML.
+        // - First Class functions?
+
+        // Object Oriented
+        // Code + Data = Object
+        // Develop to make it easier to develop, debug, reuse, and maintain software
+    }
+}
